fffa1e93ab5a272f3d281e1dc0ab7b34b7b1bc26
noah-castro <marques.de.noah@gmail.com>
2021-08-28 15:54:53 -0300

    até memset

---
+++ libft/ft_bzero.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmarques <nmarques@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/27 16:01:08 by nmarques          #+#    #+#             */
+/*   Updated: 2021/08/27 16:01:08 by nmarques         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_bzero(void *b, int len)
+{
+	int index;
+	index = 0;
+
+	int nuller;
+	nuller = '\0';
+
+
+	unsigned char *p = b;
+	while(index < len)
+	{
+		p[index] = nuller;
+		index++;
+	}
+}
--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -9,3 +9,18 @@
 /*   Updated: 2021/08/26 18:08:46 by nmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+
+void	*ft_memset(void *b, int c, int len)
+{
+	int index;
+
+	index = 0;
+	unsigned char *p = b;
+	while (index < len)
+	{
+		p[index] = (unsigned char)c;
+		index++;
+	}
+	return (b);
+}
